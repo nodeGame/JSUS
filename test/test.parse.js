b63b88b8afd9515ec5e0454a08197a55b2e9a983
Stefano Balietti <futur.dorko@gmail.com>
2012-07-11 18:25:24 +0200

    working on the parsing

--- test/test.parse.js
+++ test/test.parse.js
@@ -2,18 +2,27 @@ var util = require('util');
     should = require('should'),
     JSUS = require('./../jsus').JSUS;
     
-describe('ARRAY: ', function(){
+describe('PARSE: ', function(){
     
     // removeElement
     describe('#tokenize()', function(){
-        var separators = [' ', '.'];
-        var str = "Ciao mamma. Guarda come mi diverto.";
+        var separators = [' ', ','];
+        var str = "Ciao mamma, guarda come mi diverto";
         
-        it('should return element3 when removing it from the array', function(){
-           var tokens = JSUS.tokenize(str, separators);
-           console.log(tokens);
-           tokens.length.should.be.eql(6); 
+        it('should returns an array of the right length', function(){
+           var oo = JSUS.tokenize(str, separators);
+           oo.length.should.be.eql(6);
+           console.log(oo)
         });
+        
+//        it('should returns an array with the right tokens', function(){
+//        	var test = ['Ciao', 'mamma', 'guarda', 'come', 'mi', 'diverto'];
+//            var tokens = JSUS.tokenize(str, separators).should.be.eql(test);
+//        });
+//        
+//        it('should be able to limit the number of returned tokens', function(){
+//        	JSUS.tokenize(str, separators, {limit: 3}).length.should.be.eql(3); 
+//        });
    
     });
     

c260361e5130f9b8818fc38ef9f424279eb9907f
Stefano Balietti <futur.dorko@gmail.com>
2012-07-13 17:13:34 +0200

    PARSE fixed

--- test/test.parse.js
+++ test/test.parse.js
@@ -10,19 +10,17 @@ describe('PARSE: ', function(){
         var str = "Ciao mamma, guarda come mi diverto";
         
         it('should returns an array of the right length', function(){
-           var oo = JSUS.tokenize(str, separators);
-           oo.length.should.be.eql(6);
-           console.log(oo)
+           JSUS.tokenize(str, separators).length.should.be.eql(6);
         });
         
-//        it('should returns an array with the right tokens', function(){
-//        	var test = ['Ciao', 'mamma', 'guarda', 'come', 'mi', 'diverto'];
-//            var tokens = JSUS.tokenize(str, separators).should.be.eql(test);
-//        });
-//        
-//        it('should be able to limit the number of returned tokens', function(){
-//        	JSUS.tokenize(str, separators, {limit: 3}).length.should.be.eql(3); 
-//        });
+        it('should returns an array with the right tokens', function(){
+        	var test = ['Ciao', 'mamma', 'guarda', 'come', 'mi', 'diverto'];
+            var tokens = JSUS.tokenize(str, separators).should.be.eql(test);
+        });
+        
+        it('should be able to limit the number of returned tokens', function(){
+        	JSUS.tokenize(str, separators, {limit: 3}).length.should.be.eql(3); 
+        });
    
     });
     

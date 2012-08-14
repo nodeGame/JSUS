042e95371567b38d45b41e4c8f377df05758af90
Stefano Balietti <futur.dorko@gmail.com>
2012-08-14 16:59:33 +0200

    small changes

---
+++ test/test.dom.js
@@ -0,0 +1,30 @@
+var util = require('util');
+    should = require('should'),
+    JSUS = require('./../jsus').JSUS;
+    
+describe('DOM: ', function(){
+    
+    // removeElement
+    describe('#sprintf()', function(){
+        var separators = [' ', ','];
+        var str = "@congrats, you received %howmany tokens.";
+        var sub = {
+        		'@congrats': {
+        			style: "strong",
+        			
+        		} 
+        };
+        // TODO
+        it('should returns an array of the right length', function(){
+//           JSUS.tokenize(str, separators).length.should.be.eql(6);
+        });
+        
+        
+   
+    });
+    
+   
+    
+});
+
+

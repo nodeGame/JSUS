6f4da705e7131815022593e3aae8bfb02855e6ad
Stefano Balietti <futur.dorko@gmail.com>
2012-09-16 16:40:39 +0200

    Improved make file

--- bin/make.js
+++ bin/make.js
@@ -13,44 +13,16 @@ var program = require('commander'),
     util = require('util'),
     path = require('path'),
     exec = require('child_process').exec,
-    pkg = require('../package.json'),
-    version = pkg.version;
+    J = require('../jsus.js').JSUS;
 
+var pkg = require('../package.json'),
+    version = pkg.version;
 
 
 var build = require('./build.js').build;
 
-var buildDir =  __dirname + '/../build/';
-
-var deleteIfExist = function(file) {
-	file = file || filename;
-	if (path.existsSync(file)) {
-		var stats = fs.lstatSync(file);
-		if (stats.isDirectory()) {
-			fs.rmdir(file, function (err) {
-				if (err) throw err;  
-			});
-		}
-		else {
-			fs.unlink(file, function (err) {
-				if (err) throw err;  
-			});
-		}
-		
-	}
-};
-
-var cleanBuildDir = function(dir, ext) {
-	ext = ext || '.js';
-	dir = dir || buildDir;
-	if (dir[dir.length] !== '/') dir = dir + '/';
-	fs.readdir(dir, function(err, files) {
-	    files.filter(function(file) { return path.extname(file) ===  ext; })
-	         .forEach(function(file) { deleteIfExist(dir + file); });
-	    
-	    console.log('Build directory cleaned');
-	});
-}
+var rootDir = path.resolve(__dirname, '..') + '/';
+var buildDir = rootDir + 'build/';
 
 function list(val) {
 	return val.split(',');
@@ -63,7 +35,7 @@ program
 	.command('clean')
 	.description('Removes all files from build folder')
 	.action(function(){
-		cleanBuildDir();
+		J.cleanDir(buildDir);
 });
   
 program  
@@ -84,8 +56,8 @@ program
 	.action(function(){
 		console.log('Building documentation for JSUS v.' + version);
 		// http://nodejs.org/api.html#_child_processes
-		var root =  __dirname + '/../';
-		var command = root + 'node_modules/.bin/docker -i ' + root + ' jsus.js lib/ -s true -o ' + root + 'docs/';
+		var dockerDir = J.resolveModuleDir('docker');
+		var command = dockerDir + 'docker -i ' + rootDir + ' jsus.js lib/ -s true -o ' + rootDir + 'docs/';
 		var child = exec(command, function (error, stdout, stderr) {
 			util.print(stdout);
 			util.print(stderr);

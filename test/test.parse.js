1703381a1001668ff3d097665f9bd675dd8fc8df
lmis <lmiserez@student.ethz.ch>
2015-03-27 19:16:48 +0100

    added testing for the PARSE.range function;

--- test/test.parse.js
+++ test/test.parse.js
@@ -9,8 +9,8 @@ var str_null, str_und, str_func, str_func2, str_obj, str_obj_special, str_obj_pr
 var f = function(a,b) { console.log(a+b); };
 
 var f2 = function(a,b) {
-	this.a = a;
-	this.b = b;
+    this.a = a;
+    this.b = b;
 };
 
 var o = {
@@ -58,12 +58,12 @@ describe('PARSE: ', function(){
         });
 
         it('should returns an array with the right tokens', function(){
-        	var test = ['Ciao', 'mamma', 'guarda', 'come', 'mi', 'diverto'];
+            var test = ['Ciao', 'mamma', 'guarda', 'come', 'mi', 'diverto'];
             var tokens = JSUS.tokenize(str, separators).should.be.eql(test);
         });
 
         it('should be able to limit the number of returned tokens', function(){
-        	JSUS.tokenize(str, separators, {limit: 3}).length.should.be.eql(3);
+            JSUS.tokenize(str, separators, {limit: 3}).length.should.be.eql(3);
         });
 
     });
@@ -102,14 +102,14 @@ describe('PARSE: ', function(){
             str_obj_special = JSUS.stringify(os);
 
             var str = '{"a":"a","b":"'
-		+ JSUS.stringify_prefix + 'null","c":"'
-		+ JSUS.stringify_prefix + 'undefined","d":"'
-		+ JSUS.stringify_prefix + 'function (a,b) { console.log(a+b); }","e":"'
-		+ JSUS.stringify_prefix + 'function (a,b) { this.a = a; this.b = b; }\","f":"'
-		+ JSUS.stringify_prefix + 'NaN","g":"'
-		+ JSUS.stringify_prefix + 'Infinity","h":"'
-		+ JSUS.stringify_prefix + '-Infinity"'
-		+ '}';
+        + JSUS.stringify_prefix + 'null","c":"'
+        + JSUS.stringify_prefix + 'undefined","d":"'
+        + JSUS.stringify_prefix + 'function (a,b) { console.log(a+b); }","e":"'
+        + JSUS.stringify_prefix + 'function (a,b) { this.a = a; this.b = b; }\","f":"'
+        + JSUS.stringify_prefix + 'NaN","g":"'
+        + JSUS.stringify_prefix + 'Infinity","h":"'
+        + JSUS.stringify_prefix + '-Infinity"'
+        + '}';
 
             str_obj_special.should.be.eql(str);
         });
@@ -118,12 +118,59 @@ describe('PARSE: ', function(){
 
     describe('#stringifyAll()', function(){
 
-	it('should stringify an object with properties of the prototype', function(){
-            str_obj_proto = JSUS.stringifyAll(obj_with_proto);
-	    var str_method = '{"iamamethod":"!?_function (a){\\n    return a;\\n}"}';
-	    str_obj_proto.should.be.eql(str_method);
-	});
+        it('should stringify an object with properties of the prototype', function(){
+                str_obj_proto = JSUS.stringifyAll(obj_with_proto);
+            var str_method = '{"iamamethod":"!?_function (a){\\n    return a;\\n}"}';
+            str_obj_proto.should.be.eql(str_method);
+        });
     });
+
+    describe('#range()', function() {
+        it('should handle multiplications of constants, intervals' +
+            'negations, range notation and greater than notation',
+            function() {
+                var range = JSUS.range('2...5, >8 & !11','[-2,2*6]');
+                range.should.eql([2,3,4,5,9,10,12]);
+        });
+        it('should understand \'begin\' and \'end\', handle divisions and' +
+            'modulo operations.', function() {
+            var range = JSUS.range('begin...end/2 | 3*end/4...3...end',
+                '[0,40) & %2 = 1');
+            range.should.eql([1,3,5,7,9,11,13,15,17,19,29,35]);
+        });
+        it('should handle modulo operations in short notation', function() {
+            var range = JSUS.range('<=19, 22, %5', '>6 & !>27');
+            range.should.eql([7,8,9,10,11,12,13,14,15,16,17,18,19,20,22,25]);
+        });
+        it('should handle an iterable object as second input', function() {
+            var range = JSUS.range('>4', {
+                begin: 0,
+                end: 21,
+                prev: 0,
+                cur: 1,
+                next: function() {
+                    var temp = this.prev;
+                    this.prev = this.cur;
+                    this.cur += temp;
+                    return this.cur;
+                },
+                isFinished: function() {
+                    return this.cur + this.prev > this.end;
+                }
+            });
+            range.should.eql([5,8,13,21]);
+        });
+        it('should understand \'*\'', function() {
+            var range = JSUS.range('*','(3,8) & !%4, 22, (10,12]');
+            range.should.eql([5,6,7,11,12,22]);
+        });
+
+        it('should handle an array as second input', function() {
+            var range = JSUS.range('%7', [6,7,8,9,12,21,25,14]);
+            range.should.eql([7,21,14]);
+        });
+    });
+
 });
 
 
@@ -136,29 +183,29 @@ describe('#parse()', function(){
     });
 
     it('should parse a stringified undefined value', function(){
-    	('undefined' === typeof JSUS.parse(str_und)).should.be.true;
+        ('undefined' === typeof JSUS.parse(str_und)).should.be.true;
     });
 
     it('should parse a stringified function', function(){
-    	var a = JSUS.parse(str_func);
-    	a.toString().should.be.eql(f.toString());
+        var a = JSUS.parse(str_func);
+        a.toString().should.be.eql(f.toString());
     });
 
     it('should parse a stringified function with public properties', function(){
-    	var a = JSUS.parse(str_func2);
-    	a.toString().should.be.eql(f2.toString());
-    	var a_object = new a("a","b");
-    	a_object.a.should.be.equal("a");
-    	a_object.b.should.be.equal("b");
+        var a = JSUS.parse(str_func2);
+        a.toString().should.be.eql(f2.toString());
+        var a_object = new a("a","b");
+        a_object.a.should.be.equal("a");
+        a_object.b.should.be.equal("b");
     });
 
     it('should parse a stringified normal object (without prefix)', function(){
-    	JSUS.parse(str_obj).should.be.eql(o);
+        JSUS.parse(str_obj).should.be.eql(o);
     });
 
     it('should parse a stringified object with special values (with prefix)', function(){
-    	var a = JSUS.parse(str_obj_special);
-    	JSUS.equals(a, os).should.be.true;
+        var a = JSUS.parse(str_obj_special);
+        JSUS.equals(a, os).should.be.true;
     });
 
 });

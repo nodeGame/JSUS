b975a9b099db044aa812f04ac1464b07672f63cb
Stefano Balietti <futur.dorko@gmail.com>
2016-04-12 09:15:29 -0400

    built

--- build/jsus.js
+++ build/jsus.js
@@ -976,7 +976,7 @@
 /**
  * # DOM
  *
- * Copyright(c) 2015 Stefano Balietti
+ * Copyright(c) 2016 Stefano Balietti
  * MIT Licensed
  *
  * Collection of static functions related to DOM manipulation
@@ -1005,6 +1005,8 @@
 (function(JSUS) {
 
     "use strict";
+
+    var onFocusChange;
 
     function DOM() {}
 
@@ -2120,6 +2122,60 @@
     };
 
     /**
+     * ### DOM.onFocusIn
+     *
+     * Registers a callback to be executed when the page acquires focus
+     *
+     * @param {function} cb Executed if page acquires focus
+     * @param {object|function} ctx Optional. Context of execution for cb
+     *
+     * @see onFocusChange
+     */
+    DOM.onFocusIn = function(cb, ctx) {
+        var origCb;
+        if ('function' !== typeof cb && null !== cb) {
+            throw new TypeError('JSUS.onFocusIn: cb must be function or null.');
+        }
+        if (ctx) {
+            if ('object' !== typeof ctx && 'function' !== typeof ctx) {
+                throw new TypeError('JSUS.onFocusIn: ctx must be object, ' +
+                                    'function or undefined.');
+            }
+            origCb = cb;
+            cb = function() { origCb.call(ctx); };
+        }
+
+        onFocusChange(cb);
+    };
+
+    /**
+     * ### DOM.onFocusOut
+     *
+     * Registers a callback to be executed when the page loses focus
+     *
+     * @param {function} cb Executed if page loses focus
+     * @param {object|function} ctx Optional. Context of execution for cb
+     *
+     * @see onFocusChange
+     */
+    DOM.onFocusOut = function(cb, ctx) {
+        var origCb;
+        if ('function' !== typeof cb && null !== cb) {
+            throw new TypeError('JSUS.onFocusOut: cb must be ' +
+                                'function or null.');
+        }
+        if (ctx) {
+            if ('object' !== typeof ctx && 'function' !== typeof ctx) {
+                throw new TypeError('JSUS.onFocusIn: ctx must be object, ' +
+                                    'function or undefined.');
+            }
+            origCb = cb;
+            cb = function() { origCb.call(ctx); };
+        }
+        onFocusChange(undefined, cb);
+    };
+
+    /**
      * ### DOM.changeTitle
      *
      * Changes title of page
@@ -2184,6 +2240,94 @@
             }
         };
     }(null);
+
+
+    // ## Helper methods
+
+    /**
+     * ### onFocusChange
+     *
+     * Helper function for DOM.onFocusIn and DOM.onFocusOut (cross-browser)
+     *
+     * Expects only one callback, either inCb, or outCb.
+     *
+     * @param {function} inCb Optional. Executed if page acquires focus
+     * @param {function} outCb Optional. Executed if page loses focus
+     *
+     * Kudos: http://stackoverflow.com/questions/1060008/
+     *   is-there-a-way-to-detect-if-a-browser-window-is-not-currently-active
+     *
+     * @see http://www.w3.org/TR/page-visibility/
+     */
+    onFocusChange = (function(document) {
+        var inFocusCb, outFocusCb, event, hidden, evtMap;
+
+        if (!document) {
+            return function() {
+                JSUS.log('onFocusChange: no document detected.');
+                return;
+            };
+        }
+
+        if ('hidden' in document) {
+            hidden = 'hidden';
+            event = 'visibilitychange';
+        }
+        else if ('mozHidden' in document) {
+            hidden = 'mozHidden';
+            event = 'mozvisibilitychange';
+        }
+        else if ('webkitHidden' in document) {
+            hidden = 'webkitHidden';
+            event = 'webkitvisibilitychange';
+        }
+        else if ('msHidden' in document) {
+            hidden = 'msHidden';
+            event = 'msvisibilitychange';
+        }
+
+        evtMap = {
+            focus: true, focusin: true, pageshow: true,
+            blur: false, focusout: false, pagehide: false
+        };
+
+        function onchange(evt) {
+            var isHidden;
+            evt = evt || window.event;
+            // If event is defined as one from event Map.
+            if (evt.type in evtMap) isHidden = evtMap[evt.type];
+            // Or use the hidden property.
+            else isHidden = this[hidden] ? true : false;
+            // Call the callback, if defined.
+            if (!isHidden) { if (inFocusCb) inFocusCb(); }
+            else { if (outFocusCb) outFocusCb(); }
+        }
+
+        return function(inCb, outCb) {
+            var onchangeCb;
+
+            if ('undefined' !== typeof inCb) inFocusCb = inCb;
+            else outFocusCb = outCb;
+
+            onchangeCb = !inFocusCb && !outFocusCb ? null : onchange;
+
+            // Visibility standard detected.
+            if (event) {
+                // Remove any pre-existing listeners.
+                document.removeEventListener(event);
+                if (onchangeCb) document.addEventListener(event, onchangeCb);
+
+            }
+            else if ('onfocusin' in document) {
+                document.onfocusin = document.onfocusout = onchangeCb;
+            }
+            // All others.
+            else {
+                window.onpageshow = window.onpagehide
+                    = window.onfocus = window.onblur = onchangeCb;
+            }
+        };
+    })('undefined' !== typeof document ? document : null);
 
     JSUS.extend(DOM);
 
